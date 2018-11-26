5d8dd2349bde8c27ef77331d043cd578481b3880
biobun <biobun@gmail.com>
2018-11-26 21:49:32 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '/projectonmaca/';
+$config['base_url'] = 'https://projectonmaca.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
